dd5937230787591246dc83a19d2ec3db36851eea
Aadarsh Tiwari <117271222+aadarsh0001@users.noreply.github.com>
2024-03-01 14:12:14 +0530

    src fonder
    src folder initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <div className="container">
+        <p className="m-0 text-center">
+        <span>Created By <a href="https://www.linkedin.com/in/aadarshtiwari/" target="_blank">Aadarsh Tiwari</a> | <span class="far fa-copyright"></span>© 2024 All rights reserved </span>
+          </p>
+      </div>
+    </footer>
+  );
+}
